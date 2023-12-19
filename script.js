86168580c05714d43836136b3a85aa675dc2abbb
JhonatanSamuel <jhonatsamuel@gmail.com>
2023-12-19 17:36:12 -0300

    icon

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ function sortearNumero() {
     const min = Math.ceil(document.querySelector('.go').value)
     const max = Math.floor(document.querySelector('.the').value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (max > min) {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        resultado.innerHTML = result
+    } else {
+        alert(' O valor mínimo tem que ser Menor que o valor máximo')
+    }
     
-    resultado.innerHTML = result
+   
 
 }
 
